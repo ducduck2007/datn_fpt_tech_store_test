a2c6d050c29c068b7948abbfbcb49736dc891dc4
ducduck2007 <ducdoquang207@gmail.com>
2026-01-13 22:25:05 +0700

    .

--- db.sql
+++ db.sql
@@ -577,3 +577,19 @@ GO
 CREATE INDEX IX_returns_order ON dbo.returns(order_id, created_at DESC);
 CREATE INDEX IX_returns_item ON dbo.returns(order_item_id);
 GO
+
+IF OBJECT_ID('dbo.system_settings', 'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.system_settings (
+        setting_key   NVARCHAR(50) NOT NULL PRIMARY KEY,
+        setting_value NVARCHAR(100) NOT NULL,
+        updated_at    DATETIME2 NOT NULL DEFAULT SYSDATETIME()
+    );
+END
+GO
+
+IF NOT EXISTS (SELECT 1 FROM dbo.system_settings WHERE setting_key = 'DEFAULT_CURRENCY')
+BEGIN
+    INSERT INTO dbo.system_settings(setting_key, setting_value) VALUES ('DEFAULT_CURRENCY', 'VND');
+END
+GO
