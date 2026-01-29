2f824c2e109f691a1a1b4f8cf4f9b18406971646
Coder9892 <jackhypergamer@gmail.com>
2026-01-29 16:04:35 +0700

    Tạo báo cáo log theo module và filter log theo change_role, userId

---
+++ retailmanagement/src/main/java/com/retailmanagement/audit/AuditReportService.java
@@ -0,0 +1,24 @@
+package com.retailmanagement.audit;
+
+import com.retailmanagement.dto.response.ModuleLogReportResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AuditReportService {
+
+    private final AuditLogRepository auditLogRepository;
+
+    public List<ModuleLogReportResponse> reportByModule(){
+        return auditLogRepository.reportByModule()
+                .stream()
+                .map(r -> new ModuleLogReportResponse(
+                        (String) r[0],
+                        ((Number) r[1]).longValue()
+                ))
+                .toList();
+    }
+}
--- retailmanagement/src/main/java/com/retailmanagement/controller/AuditLogController.java
+++ retailmanagement/src/main/java/com/retailmanagement/controller/AuditLogController.java
@@ -1,7 +1,9 @@
 package com.retailmanagement.controller;
 
 import com.retailmanagement.audit.AuditLogService;
+import com.retailmanagement.audit.AuditReportService;
 import com.retailmanagement.dto.response.AuditLogResponse;
+import com.retailmanagement.dto.response.ModuleLogReportResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
@@ -16,14 +18,30 @@ import java.util.List;
 public class AuditLogController {
 
     private final AuditLogService auditLogService;
+    private final AuditReportService reportService;
 
     @GetMapping("")
     public List<AuditLogResponse> getAllAuditLogs(){
         return auditLogService.findAll();
     }
 
-    @GetMapping("filter")
+    @GetMapping("filter/day")
     public List<AuditLogResponse> filterByDay(@RequestParam("day") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)LocalDate date){
         return auditLogService.getAuditLogsByDate(date);
+    }
+
+    @GetMapping("filter/change-role")
+    public List<AuditLogResponse> filterByChange_Role(){
+        return  auditLogService.getAuditLogsByChange_Role();
+    }
+
+    @GetMapping("filter/user_id")
+    public List<AuditLogResponse> filterByUser_Id(@RequestParam("id")Integer id){
+        return auditLogService.getAuditLogsByUser_Id(id);
+    }
+
+    @GetMapping("report/module")
+    public List<ModuleLogReportResponse> reportByModule(){
+        return reportService.reportByModule();
     }
 }
