6564fa97213a4a1d9e634181c5266f47c9a04780
Đào Anh Đức <daod1068@gmail.com>
2026-03-11 12:15:42 +0700

    admin add

--- retailmanagement/src/main/java/com/retailmanagement/repository/UserRepository.java
+++ retailmanagement/src/main/java/com/retailmanagement/repository/UserRepository.java
@@ -15,7 +15,6 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
-
     // login bằng username hoặc email
     Optional<User> findByUsernameOrEmail(String username, String email);
 }
